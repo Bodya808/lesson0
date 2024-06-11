2657d56dfaf383f3811379867e4fbac6b9000606
Bodya808 <paradise2662@gmail.com>
2024-06-11 20:41:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+print(len('luboe slovo'))
+
+
+first = 10
+second = 20
+summa = first + second
+diff = first - second
+print (summa)
+print (diff)
+
+
+first = 5, 5, 3
+second = 7, 1, 4
+third = 2, 6, 8
+mean = first + second + third
+mean = sum(mean) / len(mean)
+print (mean)
+
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print (second_string + ', ' + first_string)
+
+
+a = 2
+b = 4
+c = 7
+f = (a * b) + (a * c)
+print (f)
+f = 22 **3 / 2
+print (f)
+
+
+
