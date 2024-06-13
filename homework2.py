86a23a896d50c728355bf5c9cce9d560770e270a
Bodya808 <paradise2662@gmail.com>
2024-06-13 17:15:59 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+first_name = 12
+second_name = 1.5
+name = 'Python'
+second_count = second_name / first_name
+print(f'Курс: {name}, всего задач:{first_name}, затрачено часов:{second_name}, среднее время выполнения {second_count} часа.')
+
