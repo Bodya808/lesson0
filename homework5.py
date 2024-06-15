b81e9ec2bd5ff944b02ce4357c1ea4a02956b5a7
Bodya808 <paradise2662@gmail.com>
2024-06-15 22:28:57 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+immutable_var = tuple
+tuple_ = 1, 'Bogdan', 808, True
+print('Immutable tuple:', tuple_)
+tuple_1 = 2, False, 'privet'
+#tuple_1 [0] = 400
+print(tuple_1)
+mutable_list = ([808, True, 'Text'])
+mutable_list[0] = 909
+print('Mutable list: ', mutable_list)
+
