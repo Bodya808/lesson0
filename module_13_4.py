c604ac29854f3d927c5b5e211132d230d8edc7d6
Bodya808 <paradise2662@gmail.com>
2024-10-17 14:54:05 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,51 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+api = "Тут был api"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text="Calories")
+async def set_age(message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(ag=message.text)
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(grow=message.text)
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weig=message.text)
+    data = await state.get_data()
+    BMR = int(10 * int(data['weig']) + 6.25 * int(data['grow']) - 5 * int(data['ag']) + 5)
+    await message.answer(f'Норма калорий составляет примерно {BMR} ккал/день.')
+    await state.finish()
+
+
+@dp.message_handler()
+async def start_message(message):
+    await message.answer('Я бот помогающий твоему здоровью, введите сообщение (Calories), чтобы узнать суточную норму.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
