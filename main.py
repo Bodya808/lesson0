0f784661c13405a74834f3815c74d9f39df47d2b
Bodya808 <paradise2662@gmail.com>
2024-11-30 15:19:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from app.routers import task as task_router
-from app.routers import user as user_router
+from app.routers import task, user
 import uvicorn
 import asyncio
-
 
 app = FastAPI()
 
@@ -13,9 +11,5 @@ async def root():
     return {"message": "Welcome to Taskmanager"}
 
 
-app.include_router(task_router)
-app.include_router(user_router)
-
-
-if __name__ == "main":
-    asyncio.run(uvicorn.run(app, host="127.0.0.1", port=8000, loop="asyncio"))
+app.include_router(task.router)
+app.include_router(user.router)
